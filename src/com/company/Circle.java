50091510f6eef38fc323ac7a4e55aa2a99212952
m <mmailm@mail.ru>
2020-10-08 22:23:44 +0800

    Первый коммит

---
+++ src/com/company/Circle.java
@@ -0,0 +1,7 @@
+package com.company;
+
+public class Circle {
+    public int radius = 10;
+    public int x = 0;
+    public int y = 0;
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ArrayList<Triangle> items = new ArrayList<>();
+        items.add(new Triangle(1, 2, 2));
+        items.add(new Triangle(3, 5, 6));
+        items.add(new Triangle(6, 7, 2));
+
+
+    }
+}
